6d3b18209d82f5f2847a4c2a5b1e7b66750259f2
ugo-bertrand <ugo.bertrand@epitech.eu>
2023-03-27 11:35:49 +0200

    init front project in vue.js

--- shopy-api/app.js
+++ shopy-api/app.js
@@ -29,6 +29,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes definition
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
