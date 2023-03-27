78423aa9ac934a2c691d28196ceed3626d99cf7b
ugo-bertrand <ugo.bertrand@epitech.eu>
2023-03-27 13:58:14 +0200

    create user controller

---
+++ shopy-api/controller/UserController.js
@@ -0,0 +1,48 @@
+const db = require("./model/User.js");
+
+
+// function to create a user
+exports.createUser = (req,res) => {
+
+}
+
+// function to authenticate one user
+exports.login = (req,res) => {
+ 
+}
+
+// function to logout one user
+exports.logout = (req,res) => {
+
+}
+
+//function to activate an account
+exports.activeAccount = (req,res) => {
+
+}
+
+// function to desactivate an account
+exports.desactiveAccount = (req,res) => {
+
+}
+
+// function to find one user with an id
+exports.findOne = (req,res) => {
+
+}
+
+// fuction to find all the users in the database
+exports.findAll = (req,res) => {
+
+}
+
+//function to update one user with an id and the new data
+exports.updateOne = (req,res) => {
+
+}
+
+//function to delete one user with an id
+exports.deleteOne = (req,res) => {
+
+}
+
